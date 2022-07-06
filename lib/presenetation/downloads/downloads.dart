66711cb91ee52708df0fe75d555f6a9df338e7d1
sayanth A <sayanthsayu619@gmail.com>
2022-07-06 15:02:59 +0530

    download page majority done

--- lib/presenetation/downloads/downloads.dart
+++ lib/presenetation/downloads/downloads.dart
@@ -1,7 +1,5 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-
 import 'package:netfliks_bloc/core/colors.dart';
 import 'package:netfliks_bloc/core/constants.dart';
 import 'package:netfliks_bloc/presenetation/appbar_widget.dart';
@@ -9,11 +7,7 @@ import 'package:netfliks_bloc/presenetation/appbar_widget.dart';
 class Downloads extends StatelessWidget {
   Downloads({Key? key}) : super(key: key);
 
-  final List imageList = [
-    "http://image.tmdb.org/t/p/w500/9Rj8l6gElLpRL7Kj17iZhrT5Zuw.jpg",
-    "https://www.themoviedb.org/t/p/w500/p0lTkoo94ozBu0PZkKHlpYIu4oG.jpg",
-    "https://www.themoviedb.org/t/p/w500/oA9BhUFjCwhME0Q9xgUSIGV1pf5.jpg"
-  ];
+  final _widgetList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -26,54 +20,11 @@ class Downloads extends StatelessWidget {
           )),
       body: ListView(
         children: [
+          kHeight,
           _smartDownloads(),
           kHeight,
-          const Text(
-            'Introducing Downloads for you',
-            style: TextStyle(color: white,fontSize: 24,fontWeight: FontWeight.bold),
-          ),
-          kHeight,
-           Text(
-            
-            "We'll download a personnalised selection of\nmovies and shows for you,so there's always\nsomething to watch on your device.",
-            style: TextStyle(color: Colors.grey.withOpacity(0.5),fontSize: 20),
-          textAlign: TextAlign.center,),
-          SizedBox(
-            height: size.height / 1.5,
-            width: size.width,
-            
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Center(
-                  child: CircleAvatar(
-                    radius: size.width * .4,
-                    backgroundColor: Colors.grey.withOpacity(0.5),
-                  ),
-                ),
-                DownloadsImageWidget(
-                    imageList: imageList[0], margin: EdgeInsets.only(left: 100,bottom: 20),angle: 25,),
-                    DownloadsImageWidget(imageList: imageList[1], margin:  EdgeInsets.only(right: 100,bottom: 20),angle: -25,),
-                    DownloadsImageWidget(imageList: imageList[2], margin:  EdgeInsets.only(left: 0)),
-              ],
-            ),
-          ),
-          MaterialButton(
-              color: blue,
-              child: Text(
-                "SetUp",
-                style: TextStyle(
-                    color: white, fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              onPressed: () {}),
-          MaterialButton(
-              color: Colors.black,
-              child: Text(
-                "See what you can download",
-                style: TextStyle(
-                    color: white, fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              onPressed: () {}),
+          Section2(),
+          Section3(),
         ],
       ),
     );
@@ -81,17 +32,108 @@ class Downloads extends StatelessWidget {
 
   Row _smartDownloads() {
     return Row(
-          children: const [
-            Icon(
-              Icons.settings,
-              color: white,
-            ),
-            Text(
-              "Smart Downloads",
-              style: TextStyle(color: white),
-            )
-          ],
-        );
+      children: const [
+        Icon(
+          Icons.settings,
+          color: white,
+        ),
+        Text(
+          "Smart Downloads",
+          style: TextStyle(color: white),
+        )
+      ],
+    );
+  }
+}
+
+class Section2 extends StatelessWidget {
+  Section2({Key? key}) : super(key: key);
+  final List imageList = [
+    "http://image.tmdb.org/t/p/w500/9Rj8l6gElLpRL7Kj17iZhrT5Zuw.jpg",
+    "https://www.themoviedb.org/t/p/w500/p0lTkoo94ozBu0PZkKHlpYIu4oG.jpg",
+    "https://www.themoviedb.org/t/p/w500/oA9BhUFjCwhME0Q9xgUSIGV1pf5.jpg"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        const Text(
+          'Introducing Downloads for you',
+          style: TextStyle(
+              color: white, fontSize: 25, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        kHeight,
+        Text(
+          "We'll download a personnalised selection of\nmovies and shows for you,so there's always\nsomething to watch on your device.",
+          style: TextStyle(color: Colors.grey.withOpacity(0.5), fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: size.height / 1.5,
+          width: size.width,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  radius: size.width * .4,
+                  backgroundColor: Colors.grey.withOpacity(0.5),
+                ),
+              ),
+              DownloadsImageWidget(
+                imageList: imageList[0],
+                margin: const EdgeInsets.only(left: 100, bottom: 20),
+                angle: 25,
+              ),
+              DownloadsImageWidget(
+                imageList: imageList[1],
+                margin: const EdgeInsets.only(right: 100, bottom: 20),
+                angle: -25,
+              ),
+              DownloadsImageWidget(
+                  imageList: imageList[2],
+                  margin: const EdgeInsets.only(left: 0)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Section3 extends StatelessWidget {
+  const Section3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: MaterialButton(
+              color: blue,
+              child: const Text(
+                "SetUp",
+                style: TextStyle(
+                    color: white, fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {}),
+        ),
+        kHeight,
+        MaterialButton(
+          color: Color.fromARGB(255, 247, 240, 240),
+          child: const Text(
+            "See what you can download",
+            style: TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
   }
 }
 
